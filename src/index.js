a4cbfece3bc62f89d9457cc2b2840dcfbb590ad6
Dhayanandhan R <dhaya.r@partner.samsung.com>
2018-07-11 15:09:32 +0530

    add authentication

--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
 const path = require('path');
 const { GraphQLServer } = require('graphql-yoga');
 const { importSchema } = require('graphql-import');
+const session = require('express-session');
+const ms = require('ms');
 const resolvers = require('./resolvers/resolvers');
 
 const typeDefs = importSchema(path.join(__dirname, './schemas/schema.graphql'));
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const context = req => ({
+  req: req.request
+});
+
+const server = new GraphQLServer({ typeDefs, resolvers, context });
 
 const options = {
   port: 4000,
   endpoint: '/graphql',
   subscriptions: '/subscriptions',
-  playground: '/playground'
+  playground: '/playground',
+  cors: {
+    credentials: true,
+    origin: ['http://localhost:3000']
+  }
 };
+
+server.express.use(
+  session({
+    name: 'sid',
+    secret: `superSecretSessionKey`,
+    resave: true,
+    saveUninitialized: true,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      maxAge: ms('1d')
+    }
+  })
+);
 
 server.start(options, ({ port }) => console.info(`Listening on ${port}`));
--- src/resolvers/resolvers.js
+++ src/resolvers/resolvers.js
@@ -1,5 +1,12 @@
-const RandomDie = require('./RandomDie').default;
+const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
+const RandomDie = require('./RandomDie');
+const Message = require('./Message');
 const { times } = require('../utils');
+
+const fakeDb = {};
+
+const fakeUsers = {};
 
 module.exports = {
   Query: {
@@ -11,6 +18,57 @@ module.exports = {
     whatsUp: () => 'Looking Good',
     rollDice: (_, { numDice, numSides }) =>
       times(numDice).map(() => 1 + Math.floor(Math.random() * (numSides || 6))),
-    getDie: (_, { numSides }) => new RandomDie(numSides || 6)
+    getDie: (_, { numSides }) => new RandomDie(numSides || 6),
+    getMessage: id => {
+      if (!fakeDb[id]) {
+        throw new Error('No msg with that id');
+      }
+      return new Message(id, fakeDb[id]);
+    },
+    isLogin: (_, args, { req }) => typeof req.session.user !== 'undefined',
+    loggedInUser: (_, args, { req }) => req.session.user
+  },
+  Mutation: {
+    createMessage: (_, { input }) => {
+      const id = crypto.randomBytes(10).toString('hex');
+      fakeDb[id] = input;
+      return new Message(id, input);
+    },
+    updateMessage: (_, { id, input }) => {
+      if (!fakeDb[id]) {
+        throw new Error(`No message with that id ${id}`);
+      }
+      fakeDb[id] = input;
+      return new Message(id, fakeDb[id]);
+    },
+    deleteMessage: (_, { id }) => {
+      delete fakeDb[id];
+      return 'deleted';
+    },
+    signup: async (_, { username, password }) => {
+      if (fakeUsers[username]) {
+        throw new Error(
+          `An user with ther username ${username} already exists!`
+        );
+      }
+      fakeUsers[username] = {
+        password: await bcrypt.hashSync(password, 10)
+      };
+      return true;
+    },
+    login: async (_, { username, password }, { req }) => {
+      const user = fakeUsers[username];
+      if (user) {
+        if (await bcrypt.compareSync(password, user.password)) {
+          req.session.user = {
+            ...user
+          };
+          return true;
+        }
+        throw new Error('Invalid username or password!');
+      }
+      throw new Error('No such user exists!');
+    },
+    logout: (_, args, { req }) => req.session.destroy()
   }
 };
